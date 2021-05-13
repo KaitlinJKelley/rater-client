484956528273af9ba72b86cbfa5534f4dd0c9044
kjk1325e <kaitlinjkelley@gmail.comm>
2021-05-13 10:06:16 -0500

    Added ReviewProvider and defined createReview function

---
+++ src/reviews/ReviewProvider.js
@@ -0,0 +1,19 @@
+import React, { createContext, useState } from 'react'
+
+export const ReviewContext = createContext()
+
+export const ReviewProvider = props => {
+
+    const createReview = review => {
+        return fetch(`http://localhost:8000/reviews`, {
+            method: "POST",
+            headers:{
+                "Authorization": `Token ${localStorage.getItem("lu_token")}`,
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(review)
+        })
+            .then(response => response.json())
+            // .then(getGameReviews)
+    }
+}
